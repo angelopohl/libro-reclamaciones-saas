b2babe694c705c35bb1ffdd73bb1e31c9c7816ff
angelopohl <angelfrancasanovach@gmail.com>
2026-08-05 16:36:32 -0500

    feat(Reclamo): define codigo para ReclamoDTO.

--- libro-reclamaciones-api/src/main/java/com/saas/libroreclamacionesapi/dto/ReclamoDTO.java
+++ libro-reclamaciones-api/src/main/java/com/saas/libroreclamacionesapi/dto/ReclamoDTO.java
@@ -1,4 +1,21 @@
 package com.saas.libroreclamacionesapi.dto;
 
-public record ReclamoDTO() {
-}
+import com.saas.libroreclamacionesapi.TipoBien;
+import jakarta.validation.constraints.*;
+
+public record ReclamoDTO(
+        @NotBlank
+        String tipoDocumento,
+        @NotBlank
+        String numeroDocumento,
+        @NotBlank
+        String nombreCompleto,
+        @Email
+        @NotBlank
+        String correo,
+        @NotNull
+        TipoBien tipoBien,
+        @NotBlank
+        @Size(min = 10, max = 1000)
+        String descripcion
+){}
